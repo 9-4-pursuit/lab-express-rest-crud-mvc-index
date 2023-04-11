15ebe3c7864603c73b3a28d4d80675b0682b8b92
Ari-So-Irie <arius1500@gmail.com>
2023-04-10 21:16:26 -0400

    Arius' Express REST CRUD MVC Index Lab 2

--- controllers/special-events.controller.js
+++ controllers/special-events.controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const events = express.Router();
 const eventsArray = require("../models/special-event.model");
 
-//Get all the People in the database
+//Get all the events in the database
 events.get("/", (req, res) => {
     res.json(eventsArray)
 })
