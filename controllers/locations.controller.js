e3022534661a07e15a4c88208def6c0ce2f17a81
SuperNinjaEv <evan0210@gmail.com>
2023-04-10 22:08:27 -0400

    Bonus complete, tests still passing

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -2,14 +2,14 @@ const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model");
 
-const personsArray = require("./persons.controller");
+const personsArray = require("../models/person.model");
 
 locations.get("/", (req, res) => {
     res.json(locationsArray)
 })
 
 locations.get("/people", (req, res) => {
-    let copyLocationsArray = [...personsArray];
+    let copyLocationsArray = [...locationsArray];
 
     copyLocationsArray.map((location) => {
         let copyPersonsArray = [...personsArray];
