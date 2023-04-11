3fe85e18782e8bb7812e764242e8000adf886fd7
Diegocarrero <diegocar2413@gmail.com>
2023-04-10 20:47:16 -0400

    Pass all tests again

--- app.js
+++ app.js
@@ -16,4 +16,8 @@ app.get('/', (req, res) => {
     res.send('Hello, world!');
 })
 
+app.get('*', (req, res) => {
+    res.status(404).json({ error: 'Page not found' });
+})
+
 module.exports = app;
