667bc0b604116ec2f2a9fc6856080399b9ef4873
SuperNinjaEv <evan0210@gmail.com>
2023-04-10 22:05:42 -0400

    Fixed naming issues, working on bonus

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+
 const express = require("express");
+const app = express();
+
+// Controllers
 const locationsCont = require("./controllers/locations.controller");
 const machinesCont = require("./controllers/machines.controller");
 const personsCont = require("./controllers/persons.controller");
 const plansCont = require("./controllers/plans.controller");
 const specialEventsCont = require("./controllers/special-events.controller");
 
-const app = express();
+
 app.use("/locations", locationsCont);
 app.use("/machines", machinesCont);
 app.use("/persons", personsCont);
--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -2,8 +2,22 @@ const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model");
 
+const personsArray = require("./persons.controller");
+
 locations.get("/", (req, res) => {
     res.json(locationsArray)
+})
+
+locations.get("/people", (req, res) => {
+    let copyLocationsArray = [...personsArray];
+
+    copyLocationsArray.map((location) => {
+        let copyPersonsArray = [...personsArray];
+
+        location.people = (copyPersonsArray.filter(person => person.mainLocation === location.zip));
+    });
+    res.json(copyLocationsArray);
+
 })
 
 module.exports = locations;
