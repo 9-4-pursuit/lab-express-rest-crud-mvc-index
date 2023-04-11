2650d9a9c724e9b9ce168f27e0b6519d52d6618d
JohnnyMCR <jmcastillo991@gmail.com>
2023-04-11 00:55:05 -0400

    update

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const machinesController = require("./controllers/machines.controller.js");
 const personsController = require("./controllers/persons.controller.js");
 const plansController = require("./controllers/plans.controller.js");
 const specialEventsController = require("./controllers/special-events.controller.js");
-const locationsPersonsController = require("./controllers/locations.persons.controller.js")
+// const locationsPersonsController = require("./controllers/locations.persons.controller.js")
 
 app.use("/locations", locationsController)
 app.use("/machines", machinesController)
 app.use("/persons", personsController)
 app.use("/plans", plansController)
 app.use("/special-events", specialEventsController)
-app.use("/locations-persons", locationsPersonsController)
+// app.use("/locations/people", locationsController)
 
 app.get("/", (req, res) => {
     res.send("Hello, world!")
