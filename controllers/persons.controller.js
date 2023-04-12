272305351eb16f357f819a57c00e3e043e3fc5eb
Felizj17 <felizj17@gmail.com>
2023-04-11 21:04:19 -0400

    routes created

---
+++ controllers/persons.controller.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const persons = express.Router()
+const personArray = require('../models/person.model.js')
+
+persons.get('/', (req, res) => {
+  res.json(personArray)
+})
+
+module.exports = persons
