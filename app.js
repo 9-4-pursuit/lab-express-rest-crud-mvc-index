654e8cdfb29e36d2fd6e867c486a1ae423e4e1c6
Angel <EMAIL>
2023-04-20 13:23:31 -0400

    done

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const locationsController = require("./controllers/locations.controller.js")
+const personsController = require("./controllers/persons.controller.js")
+const machinesController = require("./controllers/machines.controller.js")
+const plansController = require("./controllers/plans.controller.js")
+const eventsController = require("./controllers/special-events.controller.js")
+
+app.use("/locations", locationsController)
+
+app.use("/persons", personsController)
+
+app.use("/machines", machinesController)
+
+app.use("/plans", plansController)
+
+app.use("/special-events", eventsController)
+
+
+app.get("/", (req,res) => {
+    res.send("Hello, world!")
+})
+
+app.get("*", (req,res) => {
+    res.status(404).send("This is the error page")
+})
+
+module.exports = app
